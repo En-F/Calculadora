584820a33ee9db334e74ffe5d1f3c03425d956b7
Enrique Franco <enrique.franco@iesdonana.org>
2025-10-01 18:01:30 +0200

    Calculadora actualizada

--- auxiliar.php
+++ auxiliar.php
@@ -1,37 +1,50 @@
+<!--Recogemos los datos y luego hacemos la operacion
+La primera vez que iniciamos el codigo no  hay datos  y luego vuelve a tomar los datos 
+y estos ya existen-->
 <?php
-//Codigo que se va a utilizar en el programa principal para que sea mas limpio
-function calcular_resultado($op1,$op2,$op) 
-        {
-            switch ($op) {
-                case '+':$res = $op1 + $op2;
-                    break;
-                case '-':$res = $op1 - $op2;
-                    break;
-                case '*': $res = $op1 * $op2;
-                    break;
-                case '/': $res = $op1 / $op2;
-                    break;
-                default:
-                    $res = null;
-            }
-            return $res;
-        }
+/**
+ * Calcula el resultado de la operación definida en $op
+ * sobre los operandos $op1 y $op2 
+ */
+function calcular_resultado($op1,$op2,$op) //Creacion de la funcion,No es necesario ponerle el tipo del valor a la variable ni el valor de retorno(el que va a devolver)
+//Tenemos que evitar la repeticion de codigo
+{
+switch ($op) {
+case '+':$res = $op1 + $op2;
+break;
+case '-':$res = $op1 - $op2;
+break;
+case '*': $res = $op1 * $op2;
+break;
+case '/': $res = $op1 / $op2;
+break;
+default:
+$res = null;
+}
+return $res;
+}
 
-        /**
-         * Obtiene el valor  recogido por GET de un parámetro de la peticio.
-         * Devuelve null si el paámetro no existe.
-         */
-        function obtener_get ($parametro)
-        {
-            return isset($_GET[$parametro]) ? trim($_GET[$parametro]) :  null;
-        }
+//Validacion del tipo de numero  que lo recoge del campo 
+//que tiene el mismo  nombre. 
+//TODOS LOS DATOS TIENEN QUE SER VALIDADOS EN EL SERVIDOR
 
-        function mostrar_error ()
-        {
-            echo "<h3>El operador  es incorrecto.</h3>";
-        }
+//Comprovación de la existencia de los datos
 
-        function mostrar_resultado ($op1,$op,$op2,$res)
-        {
-            echo "<h3>El Resultado de  $op1  $op  $op2  es  $res </h3>";
-        }
+/**
+ * Obtiene el valor  recogido por GET de un parámetro de la peticio.
+ * Devuelve null si el paámetro no existe.
+ */
+function obtener_get ($parametro)
+{
+return isset($_GET[$parametro]) ? trim($_GET[$parametro]) :  null;
+}
+
+function mostrar_error ()
+{
+echo "<h3>El operador  es incorrecto.</h3>";
+}
+
+function mostrar_resultado ($op1,$op,$op2,$res)
+{
+echo "<h3>El Resultado de  $op1  $op  $op2  es  $res </h3>";
+}
--- index.php
+++ index.php
@@ -10,52 +10,6 @@
     La primera vez que iniciamos el codigo no  hay datos  y luego vuelve a tomar los datos 
     y estos ya existen-->
     <?php
-        /**
-         * Calcula el resultado de la operación definida en $op
-         * sobre los operandos $op1 y $op2 
-         */
-        function calcular_resultado($op1,$op2,$op) //Creacion de la funcion,No es necesario ponerle el tipo del valor a la variable ni el valor de retorno(el que va a devolver)
-        //Tenemos que evitar la repeticion de codigo
-        {
-            switch ($op) {
-                case '+':$res = $op1 + $op2;
-                    break;
-                case '-':$res = $op1 - $op2;
-                    break;
-                case '*': $res = $op1 * $op2;
-                    break;
-                case '/': $res = $op1 / $op2;
-                    break;
-                default:
-                    $res = null;
-            }
-            return $res;
-        }
-
-        //Validacion del tipo de numero  que lo recoge del campo 
-        //que tiene el mismo  nombre. 
-        //TODOS LOS DATOS TIENEN QUE SER VALIDADOS EN EL SERVIDOR
-        
-        //Comprovación de la existencia de los datos
-
-        /**
-         * Obtiene el valor  recogido por GET de un parámetro de la peticio.
-         * Devuelve null si el paámetro no existe.
-         */
-        function obtener_get ($parametro)
-        {
-            return isset($_GET[$parametro]) ? trim($_GET[$parametro]) :  null;
-        }
-
-        function mostrar_error ()
-        {
-            echo "<h3>El operador  es incorrecto.</h3>";
-        }
-
-        function mostrar_resultado ($op1,$op,$op2,$res)
-        {
-            echo "<h3>El Resultado de  $op1  $op  $op2  es  $res </h3>";
-        }
 
         $op1 = obtener_get('op1');
         $op2 = obtener_get('op2');
